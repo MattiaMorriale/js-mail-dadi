26f0fdbf054ae21d695ff0977ea3994692f28c3d
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-01 18:57:16 +0100

    fine dell'esercizio

--- e-mail/js/script.js
+++ e-mail/js/script.js
@@ -1,7 +1,11 @@
 // dobbiamo chiedere all'utente la sua email..
 const submitElement = document.querySelector("#submit");
-
+// dopo che lui ci da la sua mail attraverso un form,controlliamo se lui è già registrato
 const inmailElement = document.querySelector("#inmail");
+
+const avantiElement = document.querySelector("#avanti");
+
+const registratiElement = document.querySelector("#registrati");
 
 submitElement.addEventListener("click", 
     function (){
@@ -9,10 +13,11 @@ submitElement.addEventListener("click",
 
         let verifica = false;
 
+        // si controlla attraverso un for se è presente in un arrey la sua mail 
         for (let i = 0; i < mails.length ; i++){
             console.log(mails[i])
 
-            if (inmailElement === mails[i]){
+            if (inmailElement.value === mails[i]){
 
                 verifica = true;
 
@@ -22,14 +27,21 @@ submitElement.addEventListener("click",
         if (verifica) {
 
             document.getElementById("emailsuccess").innerHTML = ("La tua E-mail è presente nel nostro cloud")
+            
+            avantiElement.style.display = "block"
+
+            submitElement.style.display = "none"
 
         } else {
+            // la sua mail non sarà disponibile, e quindi apparirà un bottone che gli permetterà di registrarsi
+            document.getElementById("emailerror").innerHTML = ("La tua E-mail non è stata trovata, prego registrati")
 
-            document.getElementById("emailsuccess").innerHTML = ("La tua E-mail non è stata trovata, prego registrati")
+            registratiElement.style.display = "block"
+
+            submitElement.style.display = "none"
 
         }
     }
 )
-// dopo che lui ci da la sua mail attraverso un form,controlliamo se lui è già registrato
-// si controlla attraverso un for se è presente in un arrey la sua mail 
-// la sua mail non sarà disponibile, e quindi apparirà un bottone che gli permetterà di registrarsi
+
+
