77100a2a49bec633bee42a5d65a102d66f793940
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-01 16:07:49 +0100

    inizio dell'esercizio mail

---
+++ e-mail/js/script.js
@@ -0,0 +1,7 @@
+// dobbiamo chiedere all'utente la sua email..
+const mailElement = document.querySelector("#mail");
+mailElement = ["pippo@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "qui@gmail.com", "quo@gmail.com", "qua@gmail.com",]
+
+// dopo che lui ci da la sua mail attraverso un form,controlliamo se lui è già registrato
+// si controlla attraverso un for se è presente in un arrey la sua mail 
+// la sua mail non sarà disponibile, e quindi apparirà un bottone che gli permetterà di registrarsi
