76c94bb899683812cf28ee59bec49fd9665983a6
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-01 16:21:06 +0100

    ...

--- e-mail/js/script.js
+++ e-mail/js/script.js
@@ -1,7 +1,16 @@
 // dobbiamo chiedere all'utente la sua email..
 const mailElement = document.querySelector("#mail");
-mailElement = ["pippo@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "qui@gmail.com", "quo@gmail.com", "qua@gmail.com",]
+mailElement = ["pippo@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "qui@gmail.com", "quo@gmail.com", "qua@gmail.com"];
 
+const submitElement = document.querySelector("#submit");
+
+submitElement.addEventListener("click", 
+    function (){
+        for (let i = 0; i < 6; i++){
+            console.log(i)
+        }
+    }
+)
 // dopo che lui ci da la sua mail attraverso un form,controlliamo se lui è già registrato
 // si controlla attraverso un for se è presente in un arrey la sua mail 
 // la sua mail non sarà disponibile, e quindi apparirà un bottone che gli permetterà di registrarsi
